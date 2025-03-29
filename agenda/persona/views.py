6004c49fd3b1b0daaba6cfd073fb5af4dbe64080
Rodrigo <rodweb21@gmail.com>
2025-03-29 10:52:07 -0500

    ListAPIView

--- agenda/persona/views.py
+++ agenda/persona/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Person
+from .serializers import PersonSerializer
 
-# Create your views here.
+
+class PersonListApiView(ListAPIView):
+    serializer_class = PersonSerializer
+
+    def get_queryset(self):
+        return Person.objects.all()
