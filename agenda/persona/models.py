06bd4ffd0db934a36193285c2e523456b3bf9bcb
Rodrigo <rodweb21@gmail.com>
2025-03-30 18:32:55 -0500

    Add models with relationships

--- agenda/persona/models.py
+++ agenda/persona/models.py
@@ -2,14 +2,23 @@ from model_utils.models import TimeStampedModel
 from django.db import models
 
 
-class Person(TimeStampedModel):
-    """  Modelo para registrar personas de una agenda  """
+class Hobby(TimeStampedModel):
+    hobby = models.CharField('Pasa tiempo', max_length=50)
 
+    class Meta:
+        verbose_name = 'Hobby'
+        verbose_name_plural = 'Hobbies'
+    
+    def __str__(self):
+        return self.hobby
+
+
+class Person(TimeStampedModel):
     full_name = models.CharField('Nombres', max_length=50)
     job = models.CharField('Trabajo', max_length=30, blank=True)
     email = models.EmailField(blank=True, null=True)
     phone = models.CharField('Telefono', max_length=15, blank=True)
-
+    hobbies = models.ManyToManyField(Hobby)
 
     class Meta:
         verbose_name = 'Persona'
@@ -17,3 +26,17 @@ class Person(TimeStampedModel):
     
     def __str__(self):
         return self.full_name
+
+
+class Reunion(TimeStampedModel):
+    persona = models.ForeignKey(Person, on_delete=models.CASCADE)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    asunto = models.CharField('Aunto de Reunion', max_length=100)
+
+    class Meta:
+        verbose_name = 'Reunion'
+        verbose_name_plural = 'Reuniones'
+    
+    def __str__(self):
+        return self.asunto
